0b26fd6c7151d2ce2235f8c6700b810b52ec034f
Jaejeong <48522311+Jaejeong98@users.noreply.github.com>
2022-08-10 16:49:12 +0900

    (c#) 2022.08.09 baekjoon 5문제 완료

---
+++ 함수/함수/n_1065.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 함수
+{
+    class n_1065
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()),count=0;
+            for(int i = 100; i <=n; i++)
+            {
+                int n1 = i / 100, n2 = i % 100 / 10, n3 = i % 10;
+                if (n1 - n2 == n2 - n3)
+                    count++;
+            }
+            Console.WriteLine(count+(n<100?n:99));
+        }
+    }
+}
---
+++ 함수/함수/n_4673.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 함수
+{
+    class n_4673
+    {
+        static void _Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool[] b = new bool[10100];
+            for(int i=1; i<=10000; i++)
+            {
+                int value = i, num=i;
+                while(num > 0)
+                {
+                    value += num % 10;
+                    num /= 10;
+                }
+                b[value] = true;
+            }
+            for (int i = 1; i <= 10000; i++) {
+                if (!b[i])
+                    sb.AppendLine(i.ToString());
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
