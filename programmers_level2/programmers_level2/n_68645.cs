ba3fb5f30f1bc511ab161282b598677c87c46e41
Jaejeong <48522311+Jaejeong98@users.noreply.github.com>
2022-10-06 11:44:43 +0900

    (c#) 2022.10.04 baekjoon 1문제 완료, programmers 1문제 완료

---
+++ programmers_level2/programmers_level2/n_68645.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programmers_level2
+{
+    class n_68645
+    {
+        public int[] solution(int n)
+        {
+            int[] answer = new int[n * (n + 1) / 2];
+            int[,] arr = new int[n, n];
+            int x = -1, y = 0, num = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    if (i % 3 == 0)
+                        x++;
+                    else if (i % 3 == 1)
+                    {
+                        y++;
+                    }
+                    else
+                    {
+                        x--;
+                        y--;
+                    }
+                    arr[x, y] = num++;
+                }
+            }
+            int c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    answer[c] = arr[i, j];
+                    c++;
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ 부르트포스/부르트포스/n_2897.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 부르트포스
+{
+    class n_2897
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray(), answer=new int[5];
+            char[,] ch = new char[arr[0], arr[1]];
+            for(int i=0; i < arr[0]; i++)
+            {
+                string st = Console.ReadLine();
+                for(int j=0; j < arr[1]; j++)
+                    ch[i, j] = st[j];
+            }
+            for(int i=0; i < arr[0]-1; i++)
+            {
+                for(int j=0; j < arr[1]-1; j++)
+                {
+                    int count = 0;
+                    if (ch[i,j] == '#' || ch[i,j + 1] == '#' || ch[i + 1,j] == '#' || ch[i + 1,j + 1] == '#')
+                        continue;
+                    if (ch[i,j] == 'X')
+                        count++;
+                    if (ch[i + 1,j] == 'X')
+                        count++;
+                    if (ch[i,j + 1] == 'X')
+                        count++;
+                    if (ch[i + 1,j + 1] == 'X')
+                        count++;
+                    answer[count]++;
+                }
+            }
+            for(int i=0; i<5; i++)
+                Console.WriteLine(answer[i]);
+        }
+    }
+}
