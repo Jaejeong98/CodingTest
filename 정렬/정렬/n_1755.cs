757827ffc95c034a005327f45fbf6023a3f89eb2
Jaejeong <48522311+Jaejeong98@users.noreply.github.com>
2022-11-22 12:19:48 +0900

    (c#) 2022.11.22 baekjoon 2문제 완료

---
+++ 정렬/정렬/n_1755.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace 정렬
+{
+    class n_1755
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string[] Nums = { "zero","one","two","three","four","five","six","seven","eight","nine" };
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            StringBuilder sb = new StringBuilder();
+            for(int i = arr[0]; i <= arr[1]; i++)
+            {
+                string st = "", num=i.ToString();
+                for(int j=0; j<num.Length; j++)
+                    st += Nums[num[j]-'0'];
+                dic.Add(st, i);
+            }
+            int count = 0;
+            foreach(var item in dic.OrderBy(i => i.Key))
+            {
+                if (count != 0 && count % 10 == 0)
+                    sb.AppendLine();
+                sb.Append(item.Value+" ");
+                count++;
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ 큐/큐/n_1966.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 큐
+{
+    class n_1966
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<n; i++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray(),
+                    arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                Queue<KeyValuePair<int, int>> q = new Queue<KeyValuePair<int, int>>();
+                for(int j=0; j < arr[0]; j++)
+                    q.Enqueue(new KeyValuePair<int, int>(j, arr2[j]));
+                int count = 0;
+                while (true)
+                {
+                    int max = q.Max(x => x.Value);
+                    var deq=q.Dequeue();
+                    if (deq.Value == max)
+                    {
+                        if (deq.Key == arr[1])
+                            break;
+                        count++;
+                        continue;
+                    }
+                    q.Enqueue(deq);
+                }
+                sb.AppendLine((count+1).ToString());
+            }
+            Console.Write(sb);
+        }
+    }
+}
