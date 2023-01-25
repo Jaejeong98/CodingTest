f8c7e8821bdddcdc8748ae65cd0bad5b3fa40368
Jaejeong <48522311+Jaejeong98@users.noreply.github.com>
2023-01-25 11:52:53 +0900

    (c#) 2023.01.25 baekjoon 1문제 완료

---
+++ 정렬/정렬/n_1431.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 정렬
+{
+    class n_1431
+    {
+        public static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            string[] st=new string[T];
+            for(int i=0; i<T; i++)
+                st[i] = Console.ReadLine();
+            IEnumerable<string> list = from word in st
+                                       orderby word.Length, word.Where(x=>x>='0'&&x<='9').Sum(x=>x-'0'), word
+                                       select word;
+            foreach(string s in list)
+                Console.WriteLine(s);
+        }
+    }
+}
