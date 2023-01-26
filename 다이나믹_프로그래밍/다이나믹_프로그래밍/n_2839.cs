f9f202564451514e12b5dda6ce696e86ee8aa24a
Jaejeong <48522311+Jaejeong98@users.noreply.github.com>
2023-01-26 10:22:39 +0900

    (c#) 2023.01.26 baekjoon 1문제 완료

---
+++ 다이나믹_프로그래밍/다이나믹_프로그래밍/n_2839.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 다이나믹_프로그래밍
+{
+    class n_2839
+    {
+        public static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            int min = 0;
+            while (true)
+            {
+                if (n <= 0)
+                {
+                    Console.WriteLine(n == 0 ? min : -1);
+                    return;
+                }
+                else if (n % 5 == 0)
+                {
+                    min += n / 5;
+                    n %= 5;
+                }
+                else
+                {
+                    n -= 3;
+                    min++;
+                }
+            }
+        }
+    }
+}
